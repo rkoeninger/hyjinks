3002511088139008cf9b8014eaa7b2e21faeb889
rkoeninger <rkoeninger@att.net>
2014-06-03 23:56:55 -0400

    Set default namespace in project.clj

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "Hyjinks - An HTML generation/templating library"
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :source-paths ["./src"]
-  :test-paths ["./test"])
+  :test-paths ["./test"]
+  :main hyjinks)
