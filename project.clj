a7b7f0880cc7710ff133d6f8b02292ec05e03472
rkoeninger <rkoeninger@att.net>
2016-04-07 00:11:49 -0400

    Added cljs version of lib

--- project.clj
+++ project.clj
@@ -1,6 +1,38 @@
 (defproject hyjinks "0.0.0"
   :description "Hyjinks - An HTML generation/templating library"
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :url "http://github.com/rkoeninger/hyjinks"
+  :main hyjinks.core
   :source-paths ["src/shared"]
+  :clean-targets ^{:protect false} [
+    "target"
+    "logs"
+    "resources/public/js/compiled"]
   :test-paths ["test"]
-  :main hyjinks.core)
+  :cljsbuild {
+    :builds {
+      :dev {
+        :source-paths ["src/shared" "src/cljs"]
+        :compiler {
+          :output-to "resources/public/js/compiled/hyjinks_om.js"
+          :output-dir "resources/public/js/compiled/out"
+          :asset-path "js/compiled/out"
+          :optimizations :none
+          :main hyjinks.om
+          :source-map true
+          :source-map-timestamp true
+          :cache-analysis true}}
+      :min {
+        :source-paths ["src/shared" "src/cljs"]
+        :compiler {
+          :output-to "resources/public/js/compiled/hyjinks_om.js"
+          :main hyjinks.om
+          :optimizations :advanced
+          :pretty-print false}}}}
+  :plugins [
+    [lein-cljsbuild "1.1.1"]]
+  :dependencies [
+    [org.clojure/clojure "1.8.0"]
+    [org.clojure/clojurescript "1.8.40"]
+    [org.omcljs/om "0.9.0"]]
+  :exclusions
+    [org.clojure/clojure])
