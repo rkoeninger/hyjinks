e9496c6b8a5fb82f85b6485de0366edd0aab2a46
rkoeninger <rkoeninger@att.net>
2016-10-06 12:53:06 -0400

    Linted browser_test.js

--- browser_test.js
+++ browser_test.js
@@ -1,14 +1,19 @@
+'use strict';
+
+/*jslint node: true, browser: true, indent: 2*/
+/*global phantom*/
+
 var page = require("webpage").create();
 var url = "./resources/public/index.html";
 
 page.onConsoleMessage = function (message) {
-    console.log(message);
+  console.log(message);
 };
 
 page.onCallback = function (data) {
-    if (data && data.hasOwnProperty("exit")) {
-        phantom.exit(data.exit);
-    }
+  if (data && data.hasOwnProperty("exit")) {
+    phantom.exit(data.exit);
+  }
 };
 
 page.open(url);
