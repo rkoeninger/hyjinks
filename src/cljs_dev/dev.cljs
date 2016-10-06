82288d16dfcf8382fbd632f3223fc2d43bdf16d0
rkoeninger <rkoeninger@att.net>
2016-10-06 00:38:20 -0400

    Added simple tag->dom test

--- src/cljs_dev/dev.cljs
+++ src/cljs_dev/dev.cljs
@@ -8,18 +8,22 @@
 
 (enable-console-print!)
 
-(deftest sample-test
-  (testing "should always pass"
-    (is true))
-
-  #_ (testing "should always fail"
-    (is false)))
+(deftest dom-transform (is true)
+  (let [d (hd/tag->dom
+            (h/div {:className ["class1" "class2"]}
+              "some text"
+              (h/span {:title [:hello "!"]})
+              "more text")
+            (fn [x] (case x :hello "Hi" x)))]
+    (is (= 3 (.-length (.-childNodes d))))))
 
 (defmethod cljs.test/report [:cljs.test/default :end-run-tests] [m]
   (let [success (cljs.test/successful? m)]
     (if js/window.callPhantom
       (js/window.callPhantom #js {:exit (if success 0 1)})
-      (set! (.-innerHTML (js/document.getElementById "results")) (if success "Success" "Failure")))))
+      (js/setTimeout
+        #(set! (.-innerHTML (js/document.getElementById "results")) (if success "Success" "Failure"))
+        1000))))
 
 (set! (.-onload js/window)
   (run-tests 'hyjinks.browser.dev))
