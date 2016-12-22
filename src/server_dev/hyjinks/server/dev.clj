ffda7f6259f19e363b27c8d684e9455728157435
rkoeninger <rkoeninger@att.net>
2016-12-21 21:05:04 -0500

    Restored test server code

--- src/server_dev/hyjinks/server/dev.clj
+++ src/server_dev/hyjinks/server/dev.clj
@@ -1,10 +1,21 @@
 (ns hyjinks.server.dev
-  (:require [ring.util.response :refer [file-response]]
+  (:require [ring.util.response :refer [response]]
             [compojure.core :refer [defroutes GET]]
-            [compojure.route :refer [resources]]))
+            [compojure.route :refer [resources]]
+            [hyjinks.core :refer [tag->string html head title import-js body div css]]))
+
+(def page-frame
+  (tag->string
+    (html
+      (head
+        (title "Hyjinks Test Page")
+        (import-js "js/compiled/hyjinks_browser.js"))
+      (body
+        (div {:id "results"} (css :font-size "30px" :font-weight "bold"))
+        (div "Look at console for detailed results.")))))
 
 (defroutes app-routes
-  (GET "/" [] (file-response "resources/public/index.html"))
+  (GET "/" [] (response page-frame))
   (resources "/"))
 
 (def app #'app-routes)
