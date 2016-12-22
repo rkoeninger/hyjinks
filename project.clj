7c4f9017a03529c3c2ad170bd11b0440fa7853d4
rkoeninger <rkoeninger@att.net>
2016-12-21 20:53:39 -0500

    Replaced figwheel with ring

--- project.clj
+++ project.clj
@@ -37,22 +37,16 @@
     :test-commands {"test" ["phantomjs" "browser_test.js"]}}
   :plugins [
     [lein-cljsbuild "1.1.0"]
-    [lein-figwheel "0.4.0"]]
+    [lein-ring "0.10.0"]]
   :dependencies [
     [org.clojure/clojure "1.7.0"]
     [org.clojure/clojurescript "1.7.122"]
     [ring/ring-core "1.4.0"]
     [ring-middleware-format "0.6.0"]
     [compojure "1.4.0"]
-    [figwheel "0.4.0"]
     [org.omcljs/om "0.9.0" :exclusions [cljsjs/react]]
     [cljsjs/react-with-addons "0.13.3-0"]]
   :exclusions
     [org.clojure/clojure]
-  :figwheel {
-    :http-server-root "public" ; default and assumes "resources"
-    :server-port 3449
-    ; :nrepl-port 7888
-    :css-dirs ["resources/public/css"]
-    :ring-handler hyjinks.server.dev/app
-    :server-logfile "logs/figwheel_server.log"})
+  :ring {
+    :handler hyjinks.server.dev/app})
--- src/server_dev/hyjinks/server/dev.clj
+++ src/server_dev/hyjinks/server/dev.clj
@@ -1,19 +1,10 @@
 (ns hyjinks.server.dev
-  (:require [ring.util.response :refer [response]]
+  (:require [ring.util.response :refer [file-response]]
             [compojure.core :refer [defroutes GET]]
-            [hyjinks.core :refer [tag->string html head title import-js body div]]))
-
-(def page-frame
-  (tag->string
-    (html
-      (head
-        (title "Hyjinks Test Page")
-        (import-js "js/compiled/hyjinks_browser.js"))
-      (body
-        (div {:id "results" :style "font-size: 30px; font-weight: bold"})
-        (div "Look at console for detailed results.")))))
+            [compojure.route :refer [resources]]))
 
 (defroutes app-routes
-  (GET "/" [] (response page-frame)))
+  (GET "/" [] (file-response "resources/public/index.html"))
+  (resources "/"))
 
 (def app #'app-routes)
