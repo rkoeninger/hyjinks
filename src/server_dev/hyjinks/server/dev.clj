ea934b5703a26ab59474a42394f2485a1519ee32
rkoeninger <rkoeninger@att.net>
2016-12-24 10:50:11 -0500

    Removed html generated in server code as it's apparently not needed

--- src/server_dev/hyjinks/server/dev.clj
+++ src/server_dev/hyjinks/server/dev.clj
@@ -3,18 +3,7 @@
             [compojure.core :refer [defroutes GET]]
             [hyjinks.core :refer [tag->string html head title import-js body div css]]))
 
-(def page-frame
-  (tag->string
-    (html
-      (head
-        (title "Hyjinks Test Page")
-        (import-js "js/compiled/hyjinks_browser.js"))
-      (body
-        (div {:id "results"} (css :font-size "30px" :font-weight "bold"))
-        (div "Look at console for detailed results.")))))
-
 (defroutes app-routes
-  #_ (GET "/unit-tests" [] (response page-frame))
   (GET "/dom-demo"   [] (response "dom demo"))
   (GET "/react-demo" [] (response "react demo")))
 
