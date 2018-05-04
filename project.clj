4fb5c32d2f62d4b84fbf90e74ef9a339965a2361
Robert Koeninger <rkoeninger@att.net>
2018-05-03 23:04:51 -0400

    Deployed 0.0.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hyjinks "0.0.3"
+(defproject hyjinks "0.0.4-SNAPSHOT"
   :description "Hyjinks - An HTML generation/templating library"
   :url "http://github.com/rkoeninger/hyjinks"
   :license {
